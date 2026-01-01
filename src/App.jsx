f019e1ac60f7ea4363cb08194a9f1f6cc4c104c4
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-01-01 11:01:33 +0530

    Normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import conf from './conf/conf';
+
+function App() {
+
+  console.log(conf.appwriteDatabase);
+  
+
+  return (
+    <>
+      <h1>Jitesh</h1>
+    </>
+  )
+}
+
+export default App
