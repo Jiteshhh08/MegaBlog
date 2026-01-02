a1a1e4bf55fd35aa0efb49959edd4d2d570690f1
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-01-02 19:14:36 +0530

    started with frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import './App.css'
-import conf from './conf/conf';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import conf from "./conf/conf";
+import authService from "./appwrite/auth";
+import { Header, Footer } from "./components"
+import { login, logout } from "./store/authSlice"
+import { Outlet } from "react-router-dom"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  // console.log(conf.appwriteUrl);
-  
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    }
+  ).finally(() => setLoading(false))
+  }, []);
 
   return (
     <>
-      <h1>Jitesh</h1>
+      <div className="min-h-screen flex flex-wrap content-between bg-blue-500">
+        <div className="w-full block">
+          <Header/>
+          <main>
+            Todo: {/* <Outlet/> */}
+          </main>
+          <Footer/>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
