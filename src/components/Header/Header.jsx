a9c185b0b6ed6107fce4b8a3b50c738fdcd44f57
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-01-07 01:20:21 +0530

    Fixed all bugs in Header and logoutBtn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import { LogoutBtn, Logo, Container } from "../index";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status) 
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      url: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      url: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      url: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      url: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      url: "/add-post",
+      active: authStatus,
+    },
+  ];
+
   return (
-    <div>Header</div>
-  )
+    <>
+      <header className="shadow text-white bg-blue-950">
+        <Container>
+          <nav className="flex">
+            <div className="mr-4">
+              <Link to="/">
+                <Logo width="70px" />
+              </Link>
+            </div>
+            <ul className="flex ml-auto">
+              {
+              navItems.map( (item) => 
+                item.active ? (
+                  <li key={item.name}>
+                    <button
+                    onClick={() => {navigate(item.url)}}
+                    className = 'inline-bock px-6 py-2 duration-200 hover:bg-blue-500 rounded-xl'
+                    >
+                    {item.name}</button>
+                  </li>
+                ) : null )
+              } 
+              {authStatus && (
+                <li>
+                  <LogoutBtn/>
+                </li>
+              )} 
+            </ul>
+          </nav>
+        </Container>
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
