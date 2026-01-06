88d191c4851265506f311ee49d3bfc10b5bef28b
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-01-07 01:23:18 +0530

    Fixed some issues

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import service from "../appwrite/config"
-import {Link} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import service from "../appwrite/config";
+import { Link } from "react-router-dom";
 
-function PostCard({$id, title, featuredImage}) {
+function PostCard({ $id, title, featuredImage }) {
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    if (featuredImage) {
+      setImageUrl(service.getFilePreview(featuredImage));
+    }
+  }, [featuredImage]);
+  
   return (
-    <Link to={`/Post/$id`}>
-        <div className="bg-blue-400 p-4 rounded-xl w-full">
-            <div className="w-full justify-center mb-4">
-                <img src={service.getFilePreview(featuredImage)} alt="title" className='rounded-xl'/>
-            </div>
-            <h2 className='text-xl font-bold'>{title}</h2>
+    <Link to={`/Post/${$id}`}>
+      <div className="bg-blue-400 p-4 rounded-xl w-full">
+        <div className="w-full justify-center mb-4">
+          {imageUrl && (
+            <img
+              src={imageUrl}
+              alt={title}
+              className="w-full h-48 object-cover"
+            />
+          )}
         </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
     </Link>
-)
+  );
 }
 
-export default PostCard
+export default PostCard;
