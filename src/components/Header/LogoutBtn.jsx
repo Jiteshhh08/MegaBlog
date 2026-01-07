b3ec4b6eb3036b5a5162fa48ee1a6af8d4db46b6
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-01-07 19:44:12 +0530

    added loading feature into logoutBtn

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { logout } from '../../store/authSlice'
-import authService from '../../appwrite/auth'
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/authSlice";
+import authService from "../../appwrite/auth";
 
 function LogoutBtn() {
-    const dispatch = useDispatch()
-    const logoutHandler = () => {
-        authService.logout().then( () => {
-            dispatch(logout())
-        })
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authService
+      .logout()
+      .then(() => {
+        dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+    if (loading) {
+      return (
+        <div className="min-h-screen flex items-center justify-center">
+          <p className="text-4xl">Loading...</p>
+        </div>
+      );
     }
-  return (
-    <button className='inline-bock px-6 py-2 cursor-pointer duration-200 hover:bg-blue-500 hover:text-black rounded-full' onClick={logoutHandler}>Log out</button>
-)
+};
+    return (
+      <button
+        className="inline-bock px-6 py-2 cursor-pointer duration-200 hover:bg-blue-500 hover:text-black rounded-full"
+        onClick={logoutHandler}
+      >
+        Log out
+      </button>
+    );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
